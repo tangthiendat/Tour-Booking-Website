b7422b4c25090055d7d5562eff31d596d9860cd4
Tang Thien Dat <thiendattang@gmail.com>
2023-12-09 15:59:09 +0700

    Complete login and signup page

---
+++ js/login.js
@@ -0,0 +1,24 @@
+const loginForm = document.querySelector("#login-form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const iconShowPassword = document.querySelector(".show-password");
+
+loginForm.addEventListener("submit", function (event) {
+    const userData = localStorage.getItem(email.value);
+    event.preventDefault();
+    if (userData) {
+        const user = JSON.parse(userData);
+        if (email.value === user.email && password.value === user.password) {
+            alert("Đăng nhập thành công");
+            window.location.href = "index.html";
+        } else {
+            alert("Email hoặc mật khẩu không đúng");
+        }
+    } else {
+        alert("Email hoặc mật khẩu không đúng");
+    }
+});
+
+iconShowPassword.addEventListener("click", function () {
+    password.type = password.type === "password" ? "text" : "password";
+});
